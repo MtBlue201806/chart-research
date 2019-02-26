d948cadfaf93a016cc113024edfae993f9945b52
Naoki Aoyama <naoki.aoyama@a-saas.com>
2019-02-26 15:02:56 +0900

    c3.jsをいれた

--- src/components/charts/LibC3.vue
+++ src/components/charts/LibC3.vue
@@ -1,12 +1,42 @@
 <template>
   <div id="c3">
-    <h2 class="chart-name">C3.js</h2>    
+    <h2 class="chart-name">C3.js</h2>
+    <div class="chart" ref="c3"></div>
   </div>
 </template>
 
 <script>
+import c3 from 'c3'
 export default {
+  watch: {},
+  methods: {
+    getArgs() {
+      const data = {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ],
+        colors: {
+          data1: '#ff0000',
+          data2: '#00ff00',
+          data3: '#0000ff'
+        },
+        type: 'bar'
+      }
 
+      const size = { height: 400 }
+
+      return { data, size }
+    }
+  },
+  mounted() {
+    const args = this.getArgs()
+
+    this.$chart = c3.generate({
+      bindto: this.$refs.c3,
+      ...args
+    })
+  }
 }
 </script>
 
