af64706a85aa705924a751a6b577e467817d7c7f
Naoki Aoyama <naoki.aoyama@a-saas.com>
2019-02-28 10:51:32 +0900

    Plotly.jsでhelloworldした

--- src/components/charts/LibPlotly.vue
+++ src/components/charts/LibPlotly.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="plotly">
     <h2 class="chart-name">Plotly.js</h2>
+    <div id="chart" ref="chart"></div>
   </div>
 </template>
 
 <script>
-export default {
+import Plotly from 'plotly.js-dist'
 
+export default {
+  mounted() {
+    const chart = this.$refs.chart
+    Plotly.plot(
+      chart,
+      [
+        {
+          x: [1, 2, 3, 4, 5],
+          y: [1, 2, 4, 8, 16]
+        }
+      ],
+      {
+        margin: { t: 0 }
+      }
+    )
+  }
 }
 </script>
 
@@ -19,5 +36,10 @@ export default {
 
 .chart-name {
   margin: 1rem 0;
+}
+
+.chart {
+  width: 1000px;
+  height: 400px;
 }
 </style>
